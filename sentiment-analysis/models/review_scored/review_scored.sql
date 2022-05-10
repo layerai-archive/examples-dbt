518b34b8dfd3c95bafd2b24a2f2a0ca99f29801f
Gerard Maas <maasg@users.noreply.github.com>
2022-05-10 15:25:57 +0200

    Update sentiment-analysis/models/review_scored/review_scored.sql
    Co-authored-by: Volkan Gürel <volkangurel@users.noreply.github.com>

--- sentiment-analysis/models/review_scored/review_scored.sql
+++ sentiment-analysis/models/review_scored/review_scored.sql
@@ -6,5 +6,5 @@
 }}
 
 SELECT review,  
-predict("'layer/sentiment-analysis/models/tensorflow-sentiment-analysis:3.2'", ARRAY[review_tokenized]) as review_score
+layer.predict("'layer/sentiment-analysis/models/tensorflow-sentiment-analysis:3.2'", ARRAY[review_tokenized]) as review_score
 from {{ref('review_categories')}} 
