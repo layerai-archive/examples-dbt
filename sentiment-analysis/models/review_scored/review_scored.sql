59e5df7b6132fec938d81e7ab3fd647dae3c9f15
maasg <gerard.maas@gmail.com>
2022-05-31 14:44:50 +0200

    Add layer prefix

--- sentiment-analysis/models/review_scored/review_scored.sql
+++ sentiment-analysis/models/review_scored/review_scored.sql
@@ -1,10 +1,9 @@
 {{ config(
-		materialized='table'
-		meta={},
+		materialized='table',
 		entrypoint='predict.py'
 	) 
 }}
 
 SELECT review,  
-predict("'layer/sentiment-analysis/models/tensorflow-sentiment-analysis:3.2'", ARRAY[review_tokenized]) as review_score
-from {{ref('review_categories')}} 
+layer.predict("'layer/sentiment-analysis/models/tensorflow-sentiment-analysis:3.2'", ARRAY[review_tokenized]) as review_score
+from {{ref('review_categories')}}
